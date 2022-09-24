f888f7c71254e59dd75f84ef0350ea04e79d191f
izima01 <kingsleyizima@gmail.com>
2022-09-24 11:22:21 +0100

    fixed the issues on mobile screen

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
 const prevBtn = document.querySelector("button.prev");
 const nextBtn = document.querySelector("button.next");
-const circles = document.querySelectorAll(".circle");
+const circles = Array.from(document.getElementsByClassName("circle"));
 const progress = document.querySelector(".progress");
 let activeCircles = 1;
+
+window.onresize = checkSize;
+window.addEventListener('DOMContentLoaded', checkSize());
+
+function checkSize() {
+    circles.forEach(circle => circle.classList.remove("d-none"));
+    if (window.innerWidth <= 650 && window.innerWidth > 480) {
+        circles[7].classList.add("d-none");
+        circles[6].classList.add("d-none");
+    }
+
+    if (window.innerWidth <= 480) {
+        circles[7].classList.add("d-none");
+        circles[6].classList.add("d-none");
+        circles[5].classList.add("d-none");
+    }
+}
+let visibleCircles = circles.filter(circle => !circle.classList.contains("d-none"));
+console.log(visibleCircles);
 
 prevBtn.onclick = decreaseProgress;
 nextBtn.onclick = increaseProgress;
 
 function increaseProgress() {
     console.log("increased");
-    if (activeCircles === circles.length) {
-        activeCircles = circles.length;
+    if (activeCircles === visibleCircles.length) {
+        activeCircles = visibleCircles.length;
     } else {
         activeCircles ++;
     }
@@ -28,8 +47,8 @@ function decreaseProgress() {
 }
 
 function updateProgress () {
-    progress.style.width = `${(activeCircles-1) *Math.ceil(100/circles.length+1)}%`;
-    circles.forEach((circle, index) => {
+    progress.style.width = `${(activeCircles-1) * Math.ceil(100/visibleCircles.length+1.2)}%`;
+    visibleCircles.forEach((circle, index) => {
         if (activeCircles >= index+1) {
             circle.classList.add("active");
         } else {
